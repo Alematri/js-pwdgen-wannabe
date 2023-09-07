9aec12318f9e8910e850a0d13a0c72b20c665f87
Alematri <alessandromatricardi@gmail.com>
2023-09-07 09:50:46 +0200

    add some features

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-const nome = prompt ('inserisci il tuo nome')
-const cognome = prompt ('inserisci il tuo cognome')
-const colore = prompt ('inserisci il tuo colore preferito')
+const nome = prompt ('inserisci il tuo nome');
+const cognome = prompt ('inserisci il tuo cognome');
+const colore = prompt ('inserisci il tuo colore preferito');
 
-const message = nome + cognome + colore + '23';
+const password = nome + cognome + colore + 23;
+
+const message = 'La tua password è: ' + password;
 
 document.getElementById('output').innerHTML = message;
